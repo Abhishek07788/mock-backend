e1fc71579cac234dc84908b40dae81ccff39f4ae
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-12-15 14:28:37 +0530

    Mock-11

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/mock11");
+  return mongoose.connect(process.env.MONGOURL);
 };
 
 module.exports = connect;
