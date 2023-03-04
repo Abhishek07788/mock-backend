47b8c21be80f49a052847096637a7f7fe4f9d18f
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2023-03-04 13:37:32 +0530

    Mock-14

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/db");
-const olxRouter = require("./routes/olx.routes");
+const userRoute = require("./routes/user.routes");
 
 dotenv.config();
 let PORT = process.env.PORT || 8080;
@@ -10,10 +10,10 @@ let PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/olx", olxRouter);
+app.use("/users", userRoute);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Mock-12 B23</h1>");
+  res.send("<h1>Welcome to Mock-14 B23</h1>");
 });
 
 app.listen(PORT || 8080, async () => {
---
+++ routes/user.routes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const User = require("../model/user.model");
+const CryptoJS = require("crypto-js");
+
+const app = express.Router();
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (e) {
+    return res.send({ message: "user already exist!!", status: false });
+  }
+});
+
+// ------------- register ---------
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(404)
+        .send({ message: "user already exist!!", status: false });
+    } else {
+      await User.create({
+        email: email,
+        // Encrypt
+        password: CryptoJS.AES.encrypt(password, "!@#$%").toString(),
+      });
+      return res
+        .status(200)
+        .send({ message: "register successfully", status: true });
+    }
+  } catch (e) {
+    res.status(404).send({ Error: e.message, status: false });
+  }
+});
+
+// ------------ login ------------
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: "Wrong Credential!!", status: false });
+    }
+
+    // Decrypt
+    const decryptPass = CryptoJS.AES.decrypt(user.password, "!@#$%");
+    const originalPass = decryptPass.toString(CryptoJS.enc.Utf8);
+
+    if (originalPass === password) {
+      return res
+        .status(200)
+        .send({ message: "login successfully", status: true });
+    } else {
+      return res
+        .status(404)
+        .send({ message: "Wrong Credential!!", status: false });
+    }
+  } catch (e) {
+    res.status(404).send({ Error: e.massage, status: false });
+  }
+});
+
+module.exports = app;
